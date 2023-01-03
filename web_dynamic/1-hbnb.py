99ab1246b092358614a03d5ba4ead670fccb03b7
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-02 22:59:21 -0500

    updated 2023/01/02_20:59

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb/', strict_slashes=False)
+@app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,12 +34,13 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    cache_id = str(uuid.uuid4())
 
     return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid.uuid4())
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
