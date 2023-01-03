ca5e8d928a5d10eafffa6c04836615cdac1c84ce
Luiyi_FHB <luiyifernando5@gmail.com>
2023-01-03 14:06:47 -0500

    [TASK 3]: create a API STATUS

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,4 +13,13 @@ $(document).ready(function () {
     }
     $('.amenities h4').text(nameAmeinty.join(', '));
   });
+  $(function () {
+    $.get('http://localhost:5001/api/v1/status/', function (data, response) {
+      if (response === 200 && data.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
+  })
 });
