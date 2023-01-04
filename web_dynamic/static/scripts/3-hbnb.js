2eef763e91f27e5a8758057f2db00be41a66fd2f
Luiyi_FHB <luiyifernando5@gmail.com>
2023-01-03 22:21:49 -0500

    [TASK 4]: create a places with the front-end

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -13,12 +13,44 @@ $(document).ready(function () {
     }
     $('.amenities h4').text(nameAmeinty.join(', '));
   });
+
   $(function () {
     $.get('http://localhost:5001/api/v1/status/', function (data, response) {
       if (response === 200 || data.status === 'OK') {
         $('DIV#api_status').addClass('available');
       } else {
         $('DIV#api_status').removeClass('available');
+      }
+    });
+  })
+
+  $(function () {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      dataType: 'json',
+      success: function (places) {
+        for (let indx = 0; indx < places; indx++) {
+          const place = places[indx];
+          const placeArticle = $('<article></article>');
+          placeArticle.html(`
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guests</div>
+            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        `);
+          $('section.places').append(placeArticle);
+        }
       }
     });
   })
