87a9e80a11ac9e8a946126b357e393fd79a2bd5a
Luiyi_FHB <luiyifernando5@gmail.com>
2023-01-05 08:16:47 -0500

    [TASK 4]: correct use in the retunr for give html code

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -23,35 +23,27 @@ $(document).ready(function () {
       }
     });
   })
-
-  $(function () {
-    $.ajax({
-      type: 'POST',
-      url: 'http://0.0.0.0:5001/api/v1/places_search',
-      contentType: 'application/json',
-      data: JSON.stringify({}),
-      dataType: 'json',
-      success: function (places) {
-        for (let indx = 0; indx < places; indx++) {
-          const place = places[indx];
-          const placeArticle = $('<article></article>');
-          placeArticle.html(`
-          <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">${place.price_by_night}</div>
-          </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest} Guests</div>
-            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-          </div>
-          <div class="description">
-            ${place.description}
-          </div>
-        `);
-          $('section.places').append(placeArticle);
-        }
-      }
-    });
-  })
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `  <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">${place.max_guest} Guests</div>
+                  <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                  <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                </div>
+                <div class="description">
+                  ${place.description}
+                </div>`
+      }));
+    }
+  });
 });
